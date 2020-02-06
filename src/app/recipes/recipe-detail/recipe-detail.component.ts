c57d142f80302d71f31840207164192941846f3a
Ediezzle <edmore.mandikiyana@gmail.com>
2020-02-07 01:31:06 +0200

    add routing to the project and replace numerous features such as inputs, outputs, event emitters, some methods etc

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,21 +1,40 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipe.service';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.component.html",
+  styleUrls: ["./recipe-detail.component.css"]
 })
-export class RecipeDetailComponent implements OnInit {
-@Input() recipe: Recipe;
-  constructor(private recipeService: RecipeService) { }
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+  recipe: Recipe;
+  id: number;
+  paramsSubscription: Subscription;
+
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
+      this.id = +params["id"];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
   }
 
-  onAddToShoppingList(){
-    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients); 
+  //Because Angular does something in the background it's not necessary in this case since this is an Angular predefined observable. However if you create your own observables you would need to clean up since they are not automatically destroyed as does a component when you leave it
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(["edit"]);
+    //an alternative more complex way
+    //this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
+  }
 }
